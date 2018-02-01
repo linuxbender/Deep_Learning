ad6a31b50d1ebc451c40da566dddeaff1a98aba2
daniel glenn <linuxbender@gmail.com>
2018-02-02 00:07:16 +0100

    update ou noise

--- project-5/RL/agents/ou_noise.py
+++ project-5/RL/agents/ou_noise.py
@@ -6,18 +6,18 @@ import numpy as np
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, mu=None, theta=0.15, sigma=0.6):
+    def __init__(self, action_dimension, mu=None, theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
-        self.size = size
-        self.mu = mu if mu is not None else np.zeros(self.size)
+        self.action_dimension = action_dimension
+        self.mu = mu if mu is not None else np.zeros(self.action_dimension)
         self.theta = theta
         self.sigma = sigma
-        self.state = np.ones(self.size) * self.mu
+        self.state = np.ones(self.action_dimension) * self.mu
         self.reset()
 
     def reset(self):
-        """Reset the internal state (= noise) to mean (mu)."""
-        self.state = np.ones(self.size) * self.mu
+        """Reset the internal state (= noise) to mean (mu)."""        
+        self.state = np.ones(self.action_dimension) * self.mu
 
     def sample(self):
         """Update internal state and return it as a noise sample."""
@@ -25,3 +25,13 @@ class OUNoise:
         dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
         self.state = x + dx
         return self.state
+
+if __name__ == '__main__':
+    ou = OUNoise(3)
+    states = []
+    for i in range(1000):
+        states.append(ou.sample())
+    import matplotlib.pyplot as plt
+
+    plt.plot(states)
+plt.show()
